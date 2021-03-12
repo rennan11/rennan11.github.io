74c8c59baf59a18c6a236738c1b32c93a62e5178
Rennan <rennan11@github.com>
2021-03-12 14:11:59 -0300

    Atualização de Curriculo e Projetos

--- fluxos/fluxoadmin/fluxonormal/views/footer.php
+++
@@ -1,8 +0,0 @@
-</main>
-<script src="<?php echo URL_PREFIX; ?>js/jquery-3.4.1.min.js"></script>
-<script src="<?php echo URL_PREFIX; ?>js/jquery-ui-1.12.1.custom.min.js"></script>
-<script src="<?php echo URL_PREFIX; ?>js/popper.js"></script>
-<script src="<?php echo URL_PREFIX; ?>js/bootstrap.min.js"></script>
-<script src="<?php echo URL_PREFIX; ?>js/bootstrap-switch.min.js"></script>
-</body>
-</html>
--- fluxos/fluxoadmin/fluxonormal/views/voltarfluxo.php
+++
@@ -1,3 +0,0 @@
-<div class="voltar-box" style="position:fixed; left:10px; bottom:10px">
-    <a href="../"><button class="btn btn-danger">Voltar para Menu de Fluxo</button></a>
-</div>
